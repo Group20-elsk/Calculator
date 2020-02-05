9dfee45d748e84a526b864f37bc1685f9eabaebc
Josefine Rosenskjold <au602882@uni.au.dk>
2020-02-05 14:33:56 +0100

    Tilføjelse af Accumulator() og Clear()

--- CalculatorUnitTest/Calculator.cs
+++ CalculatorUnitTest/Calculator.cs
@@ -6,33 +6,43 @@ namespace CalculatorUnitTest
 {
     public class Calculator
     {
+        private double result = 0;
+        
         public double Add(double a, double b)
         {
-            double result=a + b;
+            result=a + b;
             return result;
         }
 
         public double Subtract(double a, double b)
         {
-            double result = a - b;
+            result = a - b;
             return result;
 
         }
         public double Multiply(double a, double b)
-        {
-            double result = a * b;
+        { 
+            result = a * b;
             return result;
         }
 
         public double Power(double x, double exp)
         {
-            double result = Math.Pow(x,exp);
+            result = Math.Pow(x,exp);
             return result;
         }
 
         public double Divide(double a, double b)
         {
-            return a / b;
+            result = a / b;
+            return result;
+        }
+
+        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0;
         }
     }
 }
